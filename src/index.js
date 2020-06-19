e1dd938f72d01957fa375e6d416c44b5014a2fda
Tyrone Regal <tyrone.regal@gmail.com>
2020-06-19 05:25:01 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import TodoContainer from "./Components/TodoContainer"
+import "./App.css"
+
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
