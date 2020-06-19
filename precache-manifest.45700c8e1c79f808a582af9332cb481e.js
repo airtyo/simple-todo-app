901b361fa2250803db2826595201c0443fc6e9d4
Tyrone Regal <tyrone.regal@gmail.com>
2020-06-19 05:53:51 +0200

    Updates

---
+++ precache-manifest.45700c8e1c79f808a582af9332cb481e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1c4ef103955d02e264e1981b789727",
+    "url": "/airtyo/simple-todo-app.git/index.html"
+  },
+  {
+    "revision": "27c6ab18606d044b6f24",
+    "url": "/airtyo/simple-todo-app.git/static/css/main.f247b8f5.chunk.css"
+  },
+  {
+    "revision": "caa6cd90eef022c007f8",
+    "url": "/airtyo/simple-todo-app.git/static/js/2.e68af391.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/airtyo/simple-todo-app.git/static/js/2.e68af391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c6ab18606d044b6f24",
+    "url": "/airtyo/simple-todo-app.git/static/js/main.6f152294.chunk.js"
+  },
+  {
+    "revision": "3d7417a394af51a6b8c0",
+    "url": "/airtyo/simple-todo-app.git/static/js/runtime-main.a125b43c.js"
+  }
+]);
